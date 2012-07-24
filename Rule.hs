eca77438939d00f57fa3412647eecf6ae6c63e14
Andy Morris <andy@adradh.org.uk>
2012-07-24 02:25:35 +0100

    term directives in Rule

--- Rule.hs
+++ Rule.hs
@@ -7,21 +7,32 @@ module Rule (
 import P
 import Glob
 import Attribute
+import Data.Char
 import Text.Parsec
-import Text.PrettyPrint.Free
+import Text.PrettyPrint.Free hiding (dot)
 import Control.Applicative hiding ((<|>), many)
 
-data Rule = Rule [Glob] [Attribute]
+data Rule = Term [String]
+          | Rule [Glob] [Attribute]
   deriving Show
 
 instance Pretty Rule where
+    pretty (Term ts) = "term" <+> hsep (map pretty ts)
     pretty (Rule gs attrs) =
       hsep (map pretty gs) <> ":" <+>
       hsep (punctuate "," $ map pretty attrs) <> "."
     prettyList = vsep . map pretty
 
 prules :: P [Rule]
-prules = many prule
+prules = many $ pterm <|> prule
+
+pterm :: P Rule
+pterm = fmap Term . between (word "term" ()) dot $ many1 identifier where
+  identifier = lexeme "term identifier" . many1 $ satisfy idChar
+  idChar c = not (isSpace c) && c /= '.'
 
 prule :: P Rule
 prule = liftA2 Rule (pglobs <* symbol ":") (pattributes <* symbol ".")
+
+dot :: P ()
+dot = symbol "."
